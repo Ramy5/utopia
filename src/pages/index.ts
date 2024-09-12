e6156ccbdfddfe847c014dc5b56b17e2f8aff30d
A-Hesham11 <a.hesham2215@gmail.com>
2024-09-12 05:20:56 +0300

    partners

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,6 +3,6 @@ import Home from "./Home";
 import Login from "./Login";
 import Register from "./Register";
 import StructurePages from "./StructurePages";
-import successStoryPage from "./successStoryPage";
+import successStoryPage from "./SuccessStoryPage";
 
 export { StructurePages, Home, BePartner, Login, Register, successStoryPage };
