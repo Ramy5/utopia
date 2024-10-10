a2b65d53da2e46bf925c3cd203b7c7a43aad0506
ramy sabry <ramyalsabry.dev@gmail.com>
2024-10-10 17:37:36 +0300

    add meta tag in head

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,19 @@ const queryClient = new QueryClient({
   },
 });
 
+const addFacebookMetaTag = () => {
+  const metaTag = document.createElement("meta");
+  metaTag.name = "facebook-domain-verification";
+  metaTag.content = "e6tvq66pxp24q8v9h0gphfdx2kr2n6";
+  document.head.appendChild(metaTag);
+
+  setTimeout(() => {
+    document.head.removeChild(metaTag);
+  }, 5000);
+};
+
+addFacebookMetaTag();
+
 const rootElement = document.getElementById("root");
 if (rootElement) {
   createRoot(rootElement).render(
